d2bf42f3599e55c8dc23f6b996c03ba3bb1cf7df
yokyyyq <iipogorelov@edu.hse.ru>
2021-10-16 23:26:36 +0600

    12

---
+++ module01/sem1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+namespace ConsoleApp112312312
+{
+    class Program
+    {
+       
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+    
+        }
+    }
+    }
+
