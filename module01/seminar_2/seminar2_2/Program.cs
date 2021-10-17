15e566b736543f3275f0ee369f79018de08f3bca
yokyyyq <iipogorelov@edu.hse.ru>
2021-10-17 12:18:20 +0600

    1

---
+++ module01/seminar_2/seminar2_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace seminar3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num<=3)
+            {
+                Console.WriteLine("неудовлетворительно");
+                return;
+                
+            }
+            if (num<=5)
+            {
+                Console.WriteLine("удов-но");
+                return;
+            }
+            if (num <= 7)
+            {
+                Console.WriteLine("хорошо");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("отлично");
+                return;
+            }
+        }
+    }
+}
