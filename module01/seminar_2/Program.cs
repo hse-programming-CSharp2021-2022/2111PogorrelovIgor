954965b96bee2023fe0b1f8f3b5e0b5ac46721b5
yokyyyq <iipogorelov@edu.hse.ru>
2021-10-17 12:10:41 +0600

    1

---
+++ module01/seminar_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace seminar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op;
+            double result = 0;
+            Console.WriteLine("Введите a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Что делать с выражением: -,+,/,*");
+            op = Console.ReadLine();
+            switch (op)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+            }
+            Console.WriteLine("Ответ:{0}", result);
+            Console.ReadKey();
+        }
+    }
+}
